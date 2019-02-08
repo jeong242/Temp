622fb15ae5a44b38c2e23cb6a35135420cfe1697
Minyonug Jeong <jeong242@umn.edu>
2019-02-08 17:37:12 -0600

    lstm minor changes

--- code/lstm/lstm.py
+++ code/lstm/lstm.py
@@ -8,23 +8,39 @@ Num_epochs = 480
 Batch_size = 160
 
 with open('../../data/BTC_1.json', 'r') as r:
-	BTC_1 = json.load(r)
+    BTC_1 = json.load(r)
  
-Xtrain = np.empty(shape=(len(BTC_1),1,4))
-Ytrain = np.empty(shape=(len(BTC_1),1))
+Xtrain = np.empty(shape=(len(BTC_1),120,4))
+Ytrain = np.zeros(shape=(len(BTC_1),3))
 
-for i in range(len(BTC_1)-1):
-	Xtrain[i,0,0] = BTC_1[i]['price_open']
-	Xtrain[i,0,1] = BTC_1[i]['price_close']
-	Xtrain[i,0,2] = BTC_1[i]['price_high']
-	Xtrain[i,0,3] = BTC_1[i]['price_low']
+# 'i' for increase / 'c' for constant / 'd' for decrease
+def delta(init, final):
+    diff = float(final) - float(init)
+    if diff > 0.0005:
+        return "Rise"
+    elif diff >= -0.0005:
+        return "Steady"
+    return "Fall"
 
-	Ytrain[i,0] = BTC_1[i+1]['price_open']
+for i in range(len(BTC_1)-210):
+    for j in range(i,i+120):
+        Xtrain[i,j-i,0] = float(BTC_1[j]['price_open']) / 1000
+        Xtrain[i,j-i,1] = float(BTC_1[j]['price_close']) / 1000
+        Xtrain[i,j-i,2] = float(BTC_1[j]['price_high']) / 1000
+        Xtrain[i,j-i,3] = float(BTC_1[j]['price_low']) / 1000
+    change = delta(BTC_1[i]['price_open'],BTC_1[i+210]['price_open'])
+    if change=="Fall":
+        Ytrain[i,0] = 1
+    elif change=="Steady":
+        Ytrain[i,1] = 1
+    else:
+        Ytrain[i,2] = 1
+
 
 model = Sequential()
-model.add(LSTM(259, input_shape=(1,4), go_backwards=True,
+model.add(LSTM(240, input_shape=(120,4), go_backwards=True,
 							 activation='relu',return_sequences=False))
-model.add(Dense(1))
+model.add(Dense(3))
 
 model.compile(loss='mean_squared_error', optimizer='adam',
 							metrics=['mean_squared_error'])
@@ -33,13 +49,13 @@ train_size = len(Xtrain)//Batch_size * Batch_size
 
 Xtrain, Ytrain = Xtrain[0:train_size], Ytrain[0:train_size]
 
-"""
 for i in range(Num_epochs):
 	model.fit(Xtrain, Ytrain, batch_size=Batch_size, epochs=1,
-						validation_split=0.2, verbose=0)
+						validation_split=0.2, verbose=1)
 """
 model.fit(Xtrain, Ytrain, batch_size=Batch_size, epochs=Num_epochs,
 						validation_split=0.2, verbose=2)
+"""
 
 score,_ = model.evaluate(Xtrain,Ytrain, batch_size=Batch_size, verbose=0)
 rmse = math.sqrt(score)
